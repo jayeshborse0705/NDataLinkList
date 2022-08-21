0e0150aea58cb5baf8c0db56cbc9f21052fd27f7
jayeshborse0705 <jayeshborse0705@gmail.com>
2022-08-21 10:10:41 +0530

    [jayesh]  insert Element in linklist

---
+++ DataLinklist/Linklist.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLinklist
+{
+    public class Linklist
+    {
+        internal Node head;
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head==null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next !=null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} insterted into link list", node.data);
+
+        }
+
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("linklist is empty");
+                return;
+            }
+            while (temp!=null)
+            {
+                Console.WriteLine(temp.data +"");
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ DataLinklist/Program.cs
@@ -0,0 +1,21 @@
+﻿using DataLinklist;
+using System;
+namespace DatastructureLinkList
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to linklist operation:");
+            Linklist list = new Linklist();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+
+            list.Display();
+
+
+        }
+
+    }
+}
