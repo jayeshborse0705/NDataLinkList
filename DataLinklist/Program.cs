b6a083c2eb0870d7c6a60e61846dd55a93cb9b3f
jayeshborse0705 <jayeshborse0705@gmail.com>
2022-08-21 13:44:12 +0530

    [jayesh]  delate item

--- DataLinklist/Program.cs
+++ DataLinklist/Program.cs
@@ -5,10 +5,10 @@ namespace DatastructureLinkList
     class program
     {
         static void Main(string[] args)
-        { 
-         Console.WriteLine("Welcome to linklist operation:");
+        {
+            Console.WriteLine("Welcome to linklist operation:");
             Linklist list = new Linklist();
-        list.Add(56);
+            list.Add(56);
             //list.Add(30);
             list.Add(70);
             list.InsertAtParticularPosition(2, 30);
@@ -16,9 +16,9 @@ namespace DatastructureLinkList
             list.Display();
             list.Search(40);
             list.Display();
-         Console.WriteLine("*********************last element delate");
-           // list.RemoveLastNode();
-           // list.Display();
+            Console.WriteLine("*********************last element delate");
+            // list.RemoveLastNode();
+            // list.Display();
 
 
 
@@ -26,5 +26,5 @@ namespace DatastructureLinkList
         }
 
 
-}
+    }
 }
