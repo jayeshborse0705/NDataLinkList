8442ca7faefd005715fc8a8df1cb2fdb77cd0158
jayeshborse0705 <jayeshborse0705@gmail.com>
2022-08-21 13:13:35 +0530

    [jayesh]  insert item

--- DataLinklist/Linklist.cs
+++ DataLinklist/Linklist.cs
@@ -9,6 +9,7 @@ namespace DataLinklist
     public class Linklist
     {
         internal Node head;
+        internal Node head;
         internal void Add(int data)
         {
             Node node = new Node(data);
@@ -42,6 +43,36 @@ namespace DataLinklist
                 Console.WriteLine(temp.data +"");
                 temp = temp.next;
             }
+
+        }
+        internal Node InsertAtParticularPosition(int position, int data)
+        {
+            if (position <1)
+                Console.WriteLine("Invalid Position");
+            if (position == 1)
+            {
+                var newNode = new Node(data);
+                newNode.next = this.head;
+                head = newNode;
+            }
+            else
+            {
+                while (position--!=0)
+                {
+                    if (position ==1)
+                    {
+                        Node node = new Node(data);
+                        node.next=this.head.next;
+                        head.next =node;
+                        break;
+                    }
+                    head=head.next;
+                }
+                if (position!=1)
+                    Console.WriteLine("Position out of range");
+            }
+            Console.WriteLine("Inserted value is:"+head);
+            return head;
         }
     }
 }
--- DataLinklist/Program.cs
+++ DataLinklist/Program.cs
@@ -8,11 +8,12 @@ namespace DatastructureLinkList
         {
             Console.WriteLine("Welcome to linklist operation:");
             Linklist list = new Linklist();
-            list.Add(70);
             list.Add(56);
-            list.Add(36);
-
+            //list.Add(30);
+            list.Add(70);
+            list.InsertAtParticularPosition(2, 30);
             list.Display();
+
 
 
         }
